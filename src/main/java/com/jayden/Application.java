4f928fea6d3ae7b95269776b60a329db3a2506a9
Jayden <JaydenRansom@outlook.com>
2021-06-08 22:33:36 +0800

    init

--- src/main/java/com/jayden/Application.java
+++ src/main/java/com/jayden/Application.java
@@ -1,13 +1,18 @@
 package com.jayden;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author jayden
  * @date 6/8/21
  */
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        logger.info("Hello World");
     }
 }
