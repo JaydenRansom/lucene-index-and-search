0f38a7c95d9a6a10ae8bc828f355b9407a02dec8
Jayden <JaydenRansom@outlook.com>
2021-06-08 21:55:38 +0800

    init

---
+++ src/main/java/com/jayden/Application.java
@@ -0,0 +1,12 @@
+package com.jayden;
+
+/**
+ * @author jayden
+ * @date 6/8/21
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
