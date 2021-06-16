114d4b79069b4d2cb64d3298130fa1fe581fcb56
Jayden <JaydenRansom@outlook.com>
2021-06-16 16:42:31 +0800

    index

--- src/main/java/com/jayden/search/IndexContainer.java
+++ src/main/java/com/jayden/search/IndexContainer.java
@@ -1,11 +1,17 @@
 package com.jayden.search;
 
+import org.apache.lucene.document.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 
 /**
  * Container for multi indices
@@ -21,6 +27,9 @@ public class IndexContainer {
     private final String basePath;
     private final int concurrency;
     private Map<String, IndexInstance> indices;
+
+    private ScheduledExecutorService refreshIndexExecutor;
+    private ScheduledExecutorService flushExecutor;
 
     public IndexContainer(final String basePath, final int concurrency) {
         this.basePath = basePath;
@@ -48,5 +57,49 @@ public class IndexContainer {
         logger.info("init indices from disk succeed.");
     }
 
+    private void scheduleFlush() {
+        this.flushExecutor = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread();
+                t.setName("flush-thread");
+                return t;
+            }
+        });
+    }
+
+    public void write(String index, Document document) throws IOException {
+        IndexInstance indexInstance = getIndexOrCreate(index);
+        int write = indexInstance.index(document);
+        if (write == -1) {
+            indexInstance.openIndex();
+            indexInstance.index(document);
+        }
+    }
+
+
+    private IndexInstance getIndexOrCreate(String index) throws IOException {
+        IndexInstance indexInstance = this.getIndex(index);
+        if (indexInstance != null) {
+            return indexInstance;
+        }
+        synchronized (lock) {
+            indexInstance = this.getIndex(index);
+            if (indexInstance == null) {
+                indexInstance = new IndexInstance(index, getIndexPath(index));
+                indexInstance.openIndex();
+                indices.put(index, indexInstance);
+            }
+            return indexInstance;
+        }
+    }
+
+    private String getIndexPath(String index) {
+        return this.basePath + "/" + index;
+    }
+
+    private IndexInstance getIndex(String index) {
+        return indices.get(index);
+    }
 
 }
